6b41dc283c75ab55000db47020a4e7d292a963bf
Will Masters <willmasters@users.noreply.github.com>
2018-01-04 16:40:13 +0100

    restore library schema

--- mopidy_plex/__init__.py
+++ mopidy_plex/__init__.py
@@ -27,6 +27,7 @@ class Extension(ext.Extension):
         schema['server'] = config.String()
         schema['username'] = config.String()
         schema['password'] = config.Secret()
+        schema['library_id'] = config.String()
         return schema
 
     def setup(self, registry):
